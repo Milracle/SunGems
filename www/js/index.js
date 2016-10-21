77d465953c1af32254be3426095c7c82aa751436
Milan Mendpara <milanmendpara@Milans-MacBook-Pro.local>
2016-10-21 18:27:21 +0530

    Phonegap Update

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        deviceReady();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
